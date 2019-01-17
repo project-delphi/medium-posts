cc36f9c8eb813c4391f543fba0e2cd6b8200545f
Ravi Kalia <ravkalia@gmail.com>
2019-01-16 22:44:58 -0600

    feat(pca.R): Add SVD decomposition to perform PCA

--- src/pca.R
+++ src/pca.R
@@ -2,7 +2,11 @@ pr_comp <- function(X, k = ncol(X), center=T, scale=T){
   n <- dim(X)[1]
   p <- dim(X)[2]
   ones <- rep(1,n)
-  H <- (diag(n) - (ifelse(center, (1/n) * ones %*% t(ones), 0)))
+  if(center){
+  H <- (diag(n) -  (1/n) * ones %*% t(ones))
+  } else {
+  H <- diag(n)
+  }
   X_center <- H %*% X
   covariance <- 1/(n-1) * t(X_center) %*% X_center
   if(scale) {
@@ -13,10 +17,25 @@ pr_comp <- function(X, k = ncol(X), center=T, scale=T){
  scaled_covariance <- diag(scaling) %*% covariance
   eig <- eigen(scaled_covariance)
   components <- (eig$vectors)[, 1:k]
-  explained_sd <- (eig$values)[1:k]
+  explained_variance <- (eig$values)[1:k]
   return(list(X=X, k=k, components=components,     
-    explained_sd=explained_sd))
+    explained_variance=explained_variance))
 }
+
+pr_comp_SVD <- function(X, k = ncol(X), center=T){
+  n <- dim(X)[1]
+  ones <- rep(1,n)
+  if(center){
+   H <- (diag(n) -  (1/n) * ones %*% t(ones))
+  } else {
+   H <- diag(n)
+  }
+  X_center <- H %*% X
+  factorization <- svd(X_center)
+ return(list(X=X, k=k, components=factorization$v[,1:k],     
+   explained_variance=1/(n-1)*(factorization$d^2)[1:k]))
+}
+
 
 # TEST
 testPCA <- function(){
@@ -25,12 +44,25 @@ D <- data.matrix(iris[,c(1:4)])
 pca_pr_comp <- pr_comp(D, scale=F)
 pca_prcomp <- prcomp(D)
 cat('\n Check for equivalence of pr_comp and prcomp: \n\n')
-cat('\n components:', all(round(pca_pr_comp$components - pca_prcomp$rotation,3) == 0))
-cat('\n explained_variance:', all(round(pca_pr_comp$explained_sd - pca_prcomp$sdev, 3) == 0))
+cat('\n components equal:', all(round(pca_pr_comp$components - pca_prcomp$rotation, 3) == 0))
+cat('\n explained_variance equal:', all(round(pca_pr_comp$explained_variance - pca_prcomp$sdev^2, 3) == 0))
 cat('\n')
 }
 
-testPCA()
+#testPCA()
 
+testPCA_SVD <- function(){
+data(iris)
+D <- data.matrix(iris[,c(1:4)])
+pca_pr_comp <- pr_comp_SVD(D)
+print(pca_pr_comp)
+pca_prcomp <- prcomp(D)
+cat('\n Check for equivalence of pr_comp and prcomp: \n\n')
+cat('\n components equal:', all(round(pca_pr_comp$components - pca_prcomp$rotation, 3) == 0))
+cat('\n explained_variance equal:', all(round(pca_pr_comp$explained_variance - pca_prcomp$sdev^2, 3) == 0))
+cat('\n')
+}
+
+testPCA_SVD()
 
 
